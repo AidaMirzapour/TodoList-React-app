f6d3f3e6aaccaca94e8b346f98495f72b39dfd8b
Aida <113546067+AidaMirzapour@users.noreply.github.com>
2024-02-11 13:23:31 -0800

    Update Button.js

--- src/TodoList/TasksList/Item/Button/Button.js
+++ src/TodoList/TasksList/Item/Button/Button.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Button(props) {
   return (
     <div>
       <button onClick={props.onClickBtn}>{props.img}</button>
     </div>
-  )
+  );
 }
