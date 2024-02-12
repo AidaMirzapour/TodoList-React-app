e193301e39ab7ac4317c74f3402c55118dafe8ef
Aida <113546067+AidaMirzapour@users.noreply.github.com>
2024-02-12 09:18:49 -0800

    add useEffect and update inputForm.js

--- src/TodoList/InputForm/InputForm.js
+++ src/TodoList/InputForm/InputForm.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./InputForm.module.css";
 
 export default function InputForm(props) {
+  const [data, setData] = useState(props.data ?? { title: "", desc: "" });
 
   //handle outside click
   const newRef = useRef(null);
@@ -19,16 +20,33 @@ export default function InputForm(props) {
   };
   //--
 
+  useEffect(() => {
+    props.onChangeItem(data, setData);
+  }, [data, props]);
+
+  const handleSubmitClick = (e) => {
+    if (typeof props.onSubmitForm !== "undefined")
+      props.onSubmitForm(e, props.index);
+    setData({ title: "", desc: "" });
+  };
+
   return (
-    <form className={style.InputForm} onSubmit={props.onSubmitForm}>
-      <p>{props.taskName}</p>
+    <form className={style.InputForm} onSubmit={handleSubmitClick} ref={newRef}>
+      <p>
+        {props.value} {data.title}
+      </p>
       <input
         autoFocus
         type="text"
         className={style.titleInput}
-        onChange={props.onChangeTitle}
+        onChange={(e) => setData({ title: e.target.value, desc: data.desc })}
+        value={data.title}
       />
-      <textarea className={style.descInput} onChange={props.onChangeDesc} />
+      <textarea
+        className={style.descInput}
+        onChange={(e) => setData({ title: data.title, desc: e.target.value })}
+        value={data.desc}
+      />
       <input type="submit" className={style.submitInput} value={props.value} />
     </form>
   );
