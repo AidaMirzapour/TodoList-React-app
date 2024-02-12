009a2e7ca0e57f901719e773e91973c9452a9117
Aida <113546067+AidaMirzapour@users.noreply.github.com>
2024-02-12 09:16:37 -0800

    import style for button component

--- src/TodoList/TasksList/Item/Button/Button.js
+++ src/TodoList/TasksList/Item/Button/Button.js
@@ -1,9 +1,12 @@
 import React from "react";
+import style from "./Button.module.css";
 
 export default function Button(props) {
   return (
     <div>
-      <button onClick={props.onClickBtn}>{props.img}</button>
+      <button onClick={props.onClickBtn} className={style.btn}>
+        {props.img}
+      </button>
     </div>
   );
 }
